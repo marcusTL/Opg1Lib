e96b7607bcfab73d334bcdd7cd4df35af310fe3e
marcusTL <mtlaursen1@gmail.com>
2020-09-18 10:08:03 +0200

    Add project files.

---
+++ Opg1Lib/Cykel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opg1Lib
+{
+    public class Cykel
+    {
+        private int _id;
+        private string _color;
+        private int _price;
+        private int _gear;
+
+        public Cykel()
+        {
+        }
+
+        public Cykel(int id, string color, int price, int gear)
+        {
+            Id = id;
+            Color = color;
+            Price = price;
+            Gear = gear;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        
+
+        public string Color
+        {
+            get => _color;
+            set
+            {
+                if (value.Length < 1) {throw new ArgumentOutOfRangeException();}
+                _color = value;
+            }
+            
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) { throw new ArgumentOutOfRangeException();}
+                _price = value;
+            }
+        }
+
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                if (value < 3 || value > 32) { throw new ArgumentOutOfRangeException();}
+                _gear = value;
+            }
+        }
+    }
+}
---
+++ Opg1LibTests/CykelTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opg1Lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opg1Lib.Tests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+        private Cykel c1;
+        //Method that all tests will begin with
+        [TestInitialize]
+        public void init()
+        {
+            c1 = new Cykel();
+        }
+        //The other tests give plenty of coverage when dealing with the conditions and validation
+        //of the individual properties and values in the constructor.
+        //I can therefore only think of testing whether the produced object is null or not. 
+        [TestMethod()]
+        public void ConstructerTest()
+        {
+            Assert.IsNotNull(c1);
+        }
+        [TestMethod()]
+        public void ColorTest()
+        {
+            //arrange color as "r"
+            c1.Color = "r";
+            //Assert whether c1.color is "r"
+            Assert.AreEqual(c1.Color,"r");
+            //assert whether c1.color is 1 character long
+            Assert.AreEqual(c1.Color.Length,1);
+            //asset whether the conditions work
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Color = "");
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            //arrange
+            c1.Price = 1;
+            //assert
+            Assert.AreEqual(c1.Price,1);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Price = 0);
+        }
+
+        [TestMethod()]
+        public void GearTest()
+        {
+            c1.Gear = 3;
+            Assert.AreEqual(c1.Gear,3);
+
+            c1.Gear = 32;
+            Assert.AreEqual(c1.Gear,32);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Gear = 33);
+        }
+    }
+}
